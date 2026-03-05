6d6e69bfdd88b2f4d7fd6186b29b3511afdd4978
ljp888233 <3098602817@qq.com>
2026-03-05 15:54:11 +0800

    Update hand_detector.py

--- core/hand_detector.py
+++ core/hand_detector.py
@@ -12,7 +12,7 @@ class HandDetector:
     def __init__(self,
                  static_image_mode=False,
                  max_num_hands=2,
-                 model_complexity=0,
+                 model_complexity=1,
                  min_detection_confidence=0.5,
                  min_tracking_confidence=0.5):
         """
